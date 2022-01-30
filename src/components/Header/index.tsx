989f0f98579002a99a4468cad4c5f0decd7df310
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-01-30 15:03:17 -0300

    Iniciando desafio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link'
+
 export default function Header() {
-  // TODO
+  return (
+    <header>
+      <Link href="/">
+        <img src="/logo.svg" alt="logo" />
+      </Link>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from 'next';
-
+import { FiCalendar, FiUser } from 'react-icons/fi'
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +24,30 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <div className={styles.post}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div>
+          <time>
+            <FiCalendar />
+            15 Mar 2021
+          </time>
+          <span>
+            <FiUser />
+            Joseph Oliveira
+          </span>
+        </div>
+      </div>
+    </main>
+  )
+}
 
 // export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+//   const prismic = getPrismicClient();
+//   const postsResponse = await prismic.query();
 
-//   // TODO
+//   TODO
 // };
